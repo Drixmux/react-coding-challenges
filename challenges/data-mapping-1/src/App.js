bd8a0900154cdf97c5b42813968a494d241fd5a9
Fernando Maceda <ferna.mace.34@gmail.com>
2022-03-28 13:53:05 -0400

    challenge 1 done

--- challenges/data-mapping-1/src/App.js
+++ challenges/data-mapping-1/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { TODOS_OLD, TODOS_NEW } from "./shared/data";
 
 function App() {
+  const records = JSON.parse(JSON.stringify(TODOS_OLD));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2> ToDo list </h2>
+        <table className="App-table">
+          <thead>
+            <th>Description</th>
+            <th>Status</th>
+          </thead>
+          <tbody>
+            {records.map((record) => (
+              <tr>
+                <td>{record.description}</td>
+                <td>{record.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </header>
     </div>
   );
---
+++ challenges/data-mapping-1/src/shared/data.js
@@ -0,0 +1,59 @@
+export const TODOS_OLD = [
+  {
+    id: "339053d0-ca6b-4ff3-aa3c-84d27541eb80",
+    description: "Log in to VPN",
+    status: "COMPLETED",
+  },
+  {
+    id: "d0662b55-49fd-4263-9d42-703247d0e608",
+    description: "Read emails",
+    status: "COMPLETED",
+  },
+  {
+    id: "ff17bb92-5dcd-4b6b-a5da-545e775f6a2f",
+    description: "Work on daily tasks",
+    status: "IN_PROGRESS",
+  },
+  {
+    id: "12b01456-b663-4dae-a50b-d64ce066974d",
+    description: "Attend to all meetings",
+    status: "IN_PROGRESS",
+  },
+  {
+    id: "61c823e3-acec-47b1-8fcb-560f5b8fc1eb",
+    description: "Send daily report",
+    status: "TO_DO",
+  },
+  {
+    id: "4da2a96b-1531-445a-9397-95a3c229cc88",
+    description: "Log out from VPN",
+    status: "TO_DO",
+  },
+];
+
+export const TODOS_NEW = {
+  "339053d0-ca6b-4ff3-aa3c-84d27541eb80": {
+    description: "Log in to VPN",
+    status: "COMPLETED",
+  },
+  "d0662b55-49fd-4263-9d42-703247d0e608": {
+    description: "Read emails",
+    status: "COMPLETED",
+  },
+  "ff17bb92-5dcd-4b6b-a5da-545e775f6a2f": {
+    description: "Work on daily tasks",
+    status: "IN_PROGRESS",
+  },
+  "12b01456-b663-4dae-a50b-d64ce066974d": {
+    description: "Attend to all meetings",
+    status: "IN_PROGRESS",
+  },
+  "61c823e3-acec-47b1-8fcb-560f5b8fc1eb": {
+    description: "Send daily report",
+    status: "TO_DO",
+  },
+  "4da2a96b-1531-445a-9397-95a3c229cc88": {
+    description: "Log out from VPN",
+    status: "TO_DO",
+  },
+};
